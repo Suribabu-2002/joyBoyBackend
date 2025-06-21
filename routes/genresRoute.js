1c52d73cb1ee570e251c813ff5ff324b5923f87b
Suribabu-Mynam <suribabu@salesken.ai>
2025-06-21 16:01:40 +0530

    commented

--- routes/genresRoute.js
+++ routes/genresRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { getGenres } from "../controllers/genreController.js";
 
 const router = express.Router();
-
+//genre handling
 router.get("/genres", getGenres);
 
 export default router;
